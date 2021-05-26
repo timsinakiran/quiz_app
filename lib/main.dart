0ab91c8c6bde9ddc74a6f3ac99d7533deb829ec1
Kiran Timsina <timsinakiran@gmail.com>
2021-05-26 02:11:42 -0700

    added dart constructor demo, added a question with 4 answers

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,49 @@
 import "package:flutter/material.dart";
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print("Answer Chosen");
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello'));
+    var questions = [
+      "What's your favorite color?",
+      "What's your favorite animal?",
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Quiz'),
+        ),
+        body: Column(
+          children: [
+            Text('The Question?'),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: () => print('Answer 2 chosen!'),
+              child: Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: () => print('Answer 3 chosen!'),
+              child: Text('Answer 3'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 4'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
